c366c7dbf8dc82d9a46b71e64f7881ed2776043a
indiplomaticterms <indiplomaticterms@gmail.com>
2012-10-28 06:57:01 -0700

    hw3_3

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -15,7 +15,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  match = page.body =~ /#{e1}.*#{e2}/m
+  assert match != nil
+  #flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
